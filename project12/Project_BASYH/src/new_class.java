ff5fdc114698e929eb32f7f494b2a8f1d5661305
Binenda <binendaa@yandex.ru>
2020-05-20 22:30:21 +0300

    Добавили проект

---
+++ project12/Project_BASYH/src/new_class.java
@@ -0,0 +1,9 @@
+public class new_class {
+	//לועמה גûחמגא פנוילא.
+    public static void main(String[] args) {
+    	//גûחמג פנוילא.
+        Kod f = new Kod();
+      //מעמבנאזוםטו פנוילא.
+        f.setVisible(true);
+    }
+}
